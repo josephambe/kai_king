61c38c82664734ed99f1c89d33e6f5bab3d9dff6
Amber Joseph <jamber919@gmail.com>
2019-07-14 19:43:57 +1200

    Added alert feature to register button on register page

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  username: string;
+  password: string;
+  repassword: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Register Page');
+  }
+
+  register() {
+      console.log("Username: " + this.username);
+      alert('Welcome to Kai King ' + this.username + '!')
+
+      // alert('Test');
+      if (this.username.length === 0 || this.password.length === 0 || this.repassword.length === 0) {
+        alert('Please fill in all fields');
+      } else {
+          console.log("Username: " + this.username);
+          console.log("Password: " + this.password);
+      }
+
   }
 
 }
