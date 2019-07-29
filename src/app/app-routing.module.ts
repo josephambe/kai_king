5f8ee1276eae85a4010ae6e80ee434a118ff61b1
Amber Joseph <jamber919@gmail.com>
2019-07-29 13:48:36 +1200

    Added a landing page and normalise scss file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AuthGuard } from './services/user/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/login/login_page.module#LoginPageModule' },
+  { path: '', loadChildren: './pages/landing/landing.module#LandingPageModule' },
+  { path: 'login', loadChildren: './pages/login/login_page.module#LoginPageModule' },
   // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
   { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard]},
   { path: 'table-create', loadChildren: './pages/table-create/table-create.module#TableCreatePageModule', canActivate: [AuthGuard] },
   { path: 'table-detail/:id', loadChildren: './pages/table-detail/table-detail.module#TableDetailPageModule', canActivate: [AuthGuard] },
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
 
 ];
 @NgModule({
