0419091085ec10dc8513c0e183c5e3e02a289f38
Amber Joseph <jamber919@gmail.com>
2019-08-10 12:45:22 +1200

    Spaced out images and labels for images

--- src/app/pages/table-list/table-list.page.ts
+++ src/app/pages/table-list/table-list.page.ts
@@ -42,9 +42,7 @@ export class TableListPage implements OnInit {
                               guests: this.guestList,
                           });
                       });
-
               });
-
           });
 
 
