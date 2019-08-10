c0e6db76f8e6550f60c1ccc5ad68cc5acafaec4e
Amber Joseph <jamber919@gmail.com>
2019-08-10 21:17:00 +1200

    Can change colour on tab icons when clicked

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -8,11 +8,19 @@ import {IonTabs} from '@ionic/angular';
 })
 export class TabsPage implements OnInit {
 
+  private tableIconColor = '#1A535C';
+
+
+
   @ViewChild('tabs') tabs: IonTabs
   constructor() { }
 
   ngOnInit() {
     this.tabs.select('table-list'); // sets default tab to table-list
+  }
+
+  changeTableIconColor() {
+    this.tableIconColor = '#4ECDC4';
   }
 
 
