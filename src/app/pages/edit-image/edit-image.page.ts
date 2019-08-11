fdbacc28a1226970c246c07d63f20aa869af65f9
Amber Joseph <jamber919@gmail.com>
2019-08-11 15:24:11 +1200

    Added loading feature for while photo is being uploaded

--- src/app/pages/edit-image/edit-image.page.ts
+++ src/app/pages/edit-image/edit-image.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {SafeResourceUrl} from '@angular/platform-browser';
 import {ActivatedRoute, Router} from '@angular/router';
 import { TableService } from '../../services/table/table.service';
+import {AlertController, LoadingController} from '@ionic/angular';
+
 import {forEach} from '@angular-devkit/schematics';
 
 
@@ -17,18 +19,23 @@ export class EditImagePage implements OnInit {
     public tableList: Array<any>;
     public selected: Array<any> = [];
     public selectedTable: any;
+    public loading: HTMLIonLoadingElement;
 
-    //Get value on ionChange on IonRadioGroup
-    selectedRadioGroup:any;
-    //Get value on ionSelect on IonRadio item
-    selectedRadioItem:any;
+
     photoTitle: any;
     photoDescription: any;
     photoTable: any;
 
 
 
-    constructor(private route: ActivatedRoute, private router: Router, private tableService: TableService) {
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private tableService: TableService,
+        public alertController: AlertController,
+        public loadingCtrl: LoadingController,
+
+) {
         this.route.queryParams.subscribe(params => {
             if (this.router.getCurrentNavigation().extras.state) {
                 this.photo = this.router.getCurrentNavigation().extras.state.photo;
@@ -55,73 +62,49 @@ export class EditImagePage implements OnInit {
             });
     }
 
-    Selection(name: string, table, photoTable) {
+    // Restricts selection to one table at a time
+    Selection(name: string, table) {
         this.tableList.forEach(x => {
             if (x.name !== name) {
                 x.value = !x.value;
 
                 this.selectedTable = table;
-                console.log("SELECTED: " + this.selectedTable.name + " " + this.photoTitle + " " + this.photoDescription);
             }
         });
-        //this.getChanged(table, photoTable);
-    }
-
-    getChanged(table, photoTable) {
-        // console.log(table.name);
-        // console.log(table.id);
-        // console.log(photoTable);
-        if (photoTable) {
-            this.selected.push(table);
-            console.log('LENGTH: ' + this.selected.length);
-
-        } else {
-            this.removeTableFromSelected(table);
-            console.log('LENGTH: ' + this.selected.length);
-        }
-        for (const t of this.selected) {
-            console.log('NAME: ' + t.name + ' ID: ' + t.id);
-        }
     }
 
 
-    removeTableFromSelected(table) {
-        const index: number = this.selected.indexOf(table);
-        if (index !== -1) {
-            this.selected.splice(index, 1);
-        }
-    }
-
-    getSelectedTables() {
-        for (const t of this.selected) {
-            console.log('NAME: ' + t.name + ' ID: ' + t.id);
-            return t.id;
-
-        }
-    }
-
-    uploadPhoto(
-        // photoTitle: string,
-        // photoDescription: string,
-        // photoTable: Array<any>,
-        votes: any,
+    async uploadPhoto(
+        votes: any, // feature to be implemented in next iteration
     ): void {
-        // photoTable = this.selectedTable;
         if (
             this.photoTitle === undefined ||
             this.photoDescription === undefined ||
             this.selectedTable === undefined
         ) {
-            console.log('MISSING SOMETHING');
-            // return;
+            this.presentAlert();
         }
+        this.loading = await this.loadingCtrl.create();
+        await this.loading.present();
 
         this.tableService
             .addPhoto(this.photoTitle, this.photoDescription, this.selectedTable, this.photoData, votes)
             .then(() => {
-                console.log('loading upload');
-                this.router.navigateByUrl('tabs/profile');
+                this.loading.dismiss().then(() => {
+                    // console.log('loading upload');
+                    this.router.navigateByUrl('tabs/profile');
+                });
             });
+    }
+
+    async presentAlert() {
+        const alert = await this.alertController.create({
+            header: 'Whoops!',
+            message: 'Make sure every field is filled.',
+            buttons: ['OK']
+        });
+
+        await alert.present();
     }
 
 }
